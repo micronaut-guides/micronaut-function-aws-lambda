0931c942282ce12ab401be9fde8c3271043fded3
sdelamo <sergio.delamo@softamo.com>
2018-09-03 07:29:10 +0200

    Client Specific configuration

--- complete/invoice/src/main/java/example/micronaut/AwsApiGatewayVatClient.java
+++ complete/invoice/src/main/java/example/micronaut/AwsApiGatewayVatClient.java
@@ -6,14 +6,16 @@ import io.micronaut.context.env.Environment;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.client.Client;
+import io.micronaut.retry.annotation.Retryable;
 import io.reactivex.Single;
 
 @Primary // <1>
-@Client("${awsapigateway.vatinvokeurl}") // <2>
+@Client("viesvatvalidator") // <2>
 @Requires(notEnv = Environment.TEST) // <3>
 public interface AwsApiGatewayVatClient extends VatValidator {
 
     @Override
-    @Post("/${awsapigateway.stage}") // <4>
+    @Retryable(attempts = "${vat.retry.attempts:3}", delay = "${vat.retry.delay:1s}")
+    @Post("/${micronaut.http.services.viesvatvalidator.stage}") // <4>
     Single<VatValidation> validateVat(@Body VatValidationRequest req);
 }
