c49804c15aa73d6f28b8b28192da8117da67687b
sdelamo <sergio.delamo@softamo.com>
2018-09-17 11:04:37 +0200

    don’t run graal test task in travis

--- travis-build-pr.sh
+++ travis-build-pr.sh
@@ -4,10 +4,13 @@ set -e
 export EXIT_STATUS=0
 
 echo "Executing tests for branch $TRAVIS_BRANCH"
-./gradlew --console=plain clean
 
-./gradlew --console=plain -Dgeb.env=chromeHeadless test || EXIT_STATUS=$?
+./gradlew --console=plain -Dgeb.env=chromeHeadless complete/vies-vat-validator:test || EXIT_STATUS=$?
 
 if [[ $EXIT_STATUS -ne 0 ]]; then
   exit $EXIT_STATUS
 fi
+
+./gradlew --console=plain -Dgeb.env=chromeHeadless complete/invoice:test || EXIT_STATUS=$?
+
+exit $EXIT_STATUS
