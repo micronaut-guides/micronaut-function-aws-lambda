b9b2d5a2d9cd55c4f09716a715c29a9f1199d759
Sergio del Amo <sergio.delamo@softamo.com>
2019-01-22 18:46:27 +0100

    Tests are already executed in travis-pr.sh

--- travis-build.sh
+++ travis-build.sh
@@ -3,18 +3,6 @@ set -e
 
 export EXIT_STATUS=0
 
-./gradlew --console=plain -Dgeb.env=chromeHeadless test || EXIT_STATUS=$?
-
-if [[ $EXIT_STATUS -ne 0 ]]; then
-  exit $EXIT_STATUS
-fi
-
-./gradlew --console=plain -Dgeb.env=chromeHeadless test || EXIT_STATUS=$?
-
-if [[ $EXIT_STATUS -ne 0 ]]; then
-  exit $EXIT_STATUS
-fi
-
 curl -O https://raw.githubusercontent.com/micronaut-projects/micronaut-guides/master/travis/build-guide
 chmod 777 build-guide
 
